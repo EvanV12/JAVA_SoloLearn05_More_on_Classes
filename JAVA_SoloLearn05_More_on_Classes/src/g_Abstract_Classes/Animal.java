8a9673668d2749b86af24021c2a1ac453a5ade51
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-06-27 01:13:50 +0300

    Added "Abstract Classes"

---
+++ JAVA_SoloLearn05_More_on_Classes/src/g_Abstract_Classes/Animal.java
@@ -0,0 +1,9 @@
+package g_Abstract_Classes;
+
+abstract class Animal {
+	
+	int legs = 0;
+	
+	abstract void makeSound();
+
+}
---
+++ JAVA_SoloLearn05_More_on_Classes/src/g_Abstract_Classes/Cat.java
@@ -0,0 +1,9 @@
+package g_Abstract_Classes;
+
+public class Cat extends Animal{
+	
+	public void makeSound() {
+		System.out.println("Meow");
+	}
+
+}
